7691be0bed1461b4fe4f91c15f125b2d5addf5ec
vishwakranti <87359394+vishwakranti@users.noreply.github.com>
2022-08-08 23:36:33 +1200

    Backend Task 3 - Created interface for the base class

---
+++ PizzaApp/Interfaces/IPizzaBase.cs
@@ -0,0 +1,20 @@
+﻿using PizzaApp.Models;
+
+namespace PizzaApp.Interfaces
+{
+    public interface IPizzaBase
+    {
+        string PizzaChef { get; set; }
+        string Name { get; set; }
+
+        List<string> Ingredients { get; set; }
+        List<Tuple<PizzaSizeEnums, decimal>> PizzaPrice { get; set; } 
+
+        bool IsVegetarian { get; set; }
+
+        bool IsGlutenFree { get; set; }
+        void PizzaChefName(string name);
+
+
+    }
+}
