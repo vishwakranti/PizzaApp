b1e8c5ba73b84b374aeb10d76684d02b370890cd
vishwakranti <87359394+vishwakranti@users.noreply.github.com>
2022-08-08 23:34:09 +1200

    Backend Task 1 - Created Pizza Base Class and added properties and methods

---
+++ PizzaApp/Models/PizzaBase.cs
@@ -0,0 +1,20 @@
+﻿using PizzaApp.Interfaces;
+
+namespace PizzaApp.Models
+{
+    public class PizzaBase : IPizzaBase
+    {
+        public string Name { get; set; }
+
+        public string PizzaChef { get; set; }
+        public List<string> Ingredients { get; set ; } = new List<string>();
+        public bool IsVegetarian { get; set ; }
+        public bool IsGlutenFree { get ; set; }
+        public List<Tuple<PizzaSizeEnums, decimal>> PizzaPrice { get; set; } = new List<Tuple<PizzaSizeEnums, decimal>>();
+
+        public virtual void PizzaChefName(string name = "Muhammad Ali")
+        {
+            PizzaChef = name;
+        }
+    }
+}
