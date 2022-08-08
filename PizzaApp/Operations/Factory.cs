9ee54cde5f9365d928f6cd03261139b219e3a21f
vishwakranti <87359394+vishwakranti@users.noreply.github.com>
2022-08-09 00:06:11 +1200

    Backend Task 4 - Created Factory class

---
+++ PizzaApp/Operations/Factory.cs
@@ -0,0 +1,25 @@
+﻿using PizzaApp.Models;
+
+namespace PizzaApp.Operations
+{
+    public static class Factory
+    {
+
+        public static PizzaBase GetAPizza(PizzaEnum pizzaType)
+        {
+            switch (pizzaType)
+            {
+                case PizzaEnum.Vegatarian:
+                    return new VegetarianPizza();
+                case PizzaEnum.Chicken:
+                    return new ChickenPizza();
+                case PizzaEnum.Beef:
+                    return new BeefPizza();
+                default:
+                    return new VegetarianPizza();
+
+
+            }
+        }
+    }
+}
